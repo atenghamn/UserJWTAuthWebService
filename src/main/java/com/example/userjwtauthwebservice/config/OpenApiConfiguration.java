1982a3bcbd4df4a580ebc71dbc3f7c9d3611d23f
atenghamn <alex.tenghamn@gmail.com>
2022-09-19 10:03:26 +0200

    * Implemented OpenApiConfiguration

---
+++ src/main/java/com/example/userjwtauthwebservice/config/OpenApiConfiguration.java
@@ -0,0 +1,39 @@
+package com.example.userjwtauthwebservice.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfiguration {
+
+    @Bean
+    public GroupedOpenApi api() {
+        return GroupedOpenApi.builder()
+                .group("jwtauthwebservice-api")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI()
+                .info(new Info().title("JWT API")
+                        .description("an api for doing jwt stuff...")
+                        .version("v1.0.0"))
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .servers(List.of(new Server().url("localhost:8080")));
+    }
+}
